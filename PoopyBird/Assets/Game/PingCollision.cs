051ebbf444dadf0ad38c86ec839389379eb6c827
Patrick Chenglo <patchenglo@berkeley.edu>
2016-02-13 23:28:47 -0800

    please based gods don't scew this up

---
+++ PoopyBird/Assets/Game/PingCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PingCollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Terrain")
+        {
+            print("touched terrain");
+        } else if (col.gameObject.name == "suitF01")
+        {
+            print("touched the man");
+
+        }
+    }
+}
